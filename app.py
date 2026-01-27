e7f6f5026335fc24c8f7aff5575b5b2e13f54b74
Realgym <realgymgimnasio@gmail.com>
2026-01-27 16:29:32 -0500

    Fix Render dynamic port

--- app.py
+++ app.py
@@ -277,4 +277,5 @@ def initialize_data():
         return jsonify({'success': False, 'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
